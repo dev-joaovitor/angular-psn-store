af0cbe44da58f92ee35878c3eeec952484c5d4a6
joaotorvitor <joaotorvitor1@gmail.com>
2023-08-29 19:54:53 -0300

    card-pricing component added

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  gameLabel:string = "Exclusive"
+  @Input()
+  gameLabel:string = ""
+  @Input()
+  gameCover:string = ""
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice: string = ""
 }
